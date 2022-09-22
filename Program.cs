2f496947f48e184a7104269a9f8279973bdfaf5b
kasonmills <97563963+kasonmills@users.noreply.github.com>
2022-09-22 16:13:56 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace CSE210_kasonmills
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+
